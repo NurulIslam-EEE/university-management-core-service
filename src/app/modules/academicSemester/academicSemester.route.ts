78b68d74f98524ca20886cd2f9e978d0c6b2f3e6
NurulIslam-EEE <nurul.islam02310@gmail.com>
2023-09-28 02:55:25 +0600

    insert into db , get all from db done

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemster.validation';
+
+const router = express.Router();
+
+router.get('/', AcademicSemesterController.getAllFromDB);
+
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
+
+export const AcademicSemesterRoutes = router;
